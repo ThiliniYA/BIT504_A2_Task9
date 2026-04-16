51501e5672ee83347aedcf609ddfc76b280f3dc4
ThiliniYA <thiliniyapa88@gmail.com>
2026-04-16 16:19:05 +1200

    Initial commit – added Java project and main class.

---
+++ src/Task9Demo.java
@@ -0,0 +1,10 @@
+
+public class Task9Demo {
+
+	public static void main(String[] args) {
+		System.out.println("BIT504 Task 9 - Version Control Demo");
+		System.out.println("This is my first commit.");
+
+	}
+
+}
