af84caff794cbb84e023fdfe522e7e1e794176db
ThiliniYA <thiliniyapa88@gmail.com>
2026-04-16 18:03:03 +1200

    Added third output line on feature-update2 branch.

--- src/Task9Demo.java
+++ src/Task9Demo.java
@@ -4,7 +4,9 @@ public class Task9Demo {
 	public static void main(String[] args) {
 		System.out.println("BIT504 Task 9 - Version Control Demo");
 		System.out.println("This is my first commit.");
-
+		System.out.println("This is my second commit on feature-update1.");
+		System.out.println("This is my third commit on feature-update2.");
+		
 	}
 
 }
